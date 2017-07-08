55ed0ed5a02b7c535f35a4a1e4a071589029428d
Levydor <dordeep@gmail.com>
2017-07-08 19:35:20 +0300

    Update test.rb
    *Ingredients fix

--- test.rb
+++ test.rb
@@ -1,12 +1,12 @@
 require "./recipe.rb"
 require "./tool.rb"
-require "./ingredent.rb"
+require "./ingredient.rb"
 require "./mood.rb"
 require "./category.rb"
-require "./recipe_ingredents.rb"
+require "./recipe_ingredients.rb"
 require "./recipe_tools"
 require "./kitchen_style.rb"
-
+require "./profile.rb"
 
 =begin
 
@@ -26,16 +26,16 @@ bowl = Tool.new("bowl", recipe_id)
 vagetable = Category.new(1, "vagetable")
 spice = Category.new(2, "spice")
 
-tomato = Ingredent.new("tomato", 1, 0.2, vagetable)
-black_pepper = Ingredent.new("black_pepper", 1, 0, spice)
-green_pepper = Ingredent.new("green_pepper", 2, 0.3, vagetable)
+tomato = Ingredient.new("tomato", 1, 0.2, vagetable)
+black_pepper = Ingredient.new("black_pepper", 1, 0, spice)
+green_pepper = Ingredient.new("green_pepper", 2, 0.3, vagetable)
 
 fish_tools = Recipe_tools.new([spoon, bowl], recipe_id)
-fish_ingredents = Recipe_ingredents.new([tomato, black_pepper, green_pepper], recipe_id)
+fish_ingredients = Recipe_ingredients.new([tomato, black_pepper, green_pepper], recipe_id)
 
 # I invented this - maybe not necessary:
 home_mood = Mood.new("home", recipe_id)
 maroccan_style = Kitchen_style.new("maroccan", recipe_id)
 
 
-fish = Recipe.new("maroccan fish", fish_tools, fish_ingredents, home_mood, maroccan_style, 4, 120, "hard", ["kosher"], "bla bla bla").print_recipe
+fish = Recipe.new("maroccan fish", fish_tools, fish_ingredients, home_mood, maroccan_style, 4, 120, "hard", ["kosher"], "bla bla bla").print_recipe
