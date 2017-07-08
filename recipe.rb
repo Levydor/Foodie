62f46608fae3bfe832ac25fd6ed1b4d9c6819a14
Levydor <dordeep@gmail.com>
2017-07-08 19:26:02 +0300

    Update recipe.rb
    *ingredient

--- recipe.rb
+++ recipe.rb
@@ -1,11 +1,11 @@
 # Defines a user recipe
 class Recipe
-	attr_reader :name, :recipe_id, :tools, :ingredents, :mood, :kitchen_style, :diners_amount, :cooking_time, :level, :prefrences, :description
-	def initialize(name, tools, ingredents, mood, kitchen_style, diners_amount, cooking_time, level, prefrences, description)
+	attr_reader :name, :recipe_id, :tools, :ingredients, :mood, :kitchen_style, :diners_amount, :cooking_time, :level, :prefrences, :description
+	def initialize(name, tools, ingredients, mood, kitchen_style, diners_amount, cooking_time, level, prefrences, description)
 		@name = name
 		@recipe_id = Recipe.generate_recipe_id()
 		@tools = tools
-		@ingredents = ingredents
+		@ingredients = ingredients
 		@mood = mood
 		@kitchen_style = kitchen_style
 		@diners_amount = diners_amount
@@ -32,14 +32,14 @@ class Recipe
 
 	def print_recipe
 		tool_names = []
-		ingredent_names = []
+		ingredient_names = []
 		tools.tools.each do |tool|
 			tool_names << tool.name
 		end
-		ingredents.ingredents.each do |ingredent|
-			ingredent_names << ingredent.name
+		ingredients.ingredients.each do |ingredient|
+			ingredient_names << ingredient.name
 		end
-		puts "======= Recipe for: #{name} ======= \n Tools required: #{tool_names}, \n Ingredents required: #{ingredent_names}, \n Mood required: #{mood.name} mood, \n Kitchen style: #{kitchen_style.name},\n Diners amount: #{diners_amount},\n Cooking time: #{cooking_time} minutes,\n level: #{level},\n Prefrences: #{prefrences},\n Recipe description: #{description}\n See you at the bitter end ;)"
+		puts "======= Recipe for: #{name} ======= \n Tools required: #{tool_names}, \n Ingredients required: #{ingredient_names}, \n Mood required: #{mood.name} mood, \n Kitchen style: #{kitchen_style.name},\n Diners amount: #{diners_amount},\n Cooking time: #{cooking_time} minutes,\n level: #{level},\n Prefrences: #{prefrences},\n Recipe description: #{description}\n See you at the bitter end ;)"
 	end
 end
 
